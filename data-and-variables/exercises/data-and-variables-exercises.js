e733e74f0287a4e08df71a38d0203f9f1cc8cee6
Vincent Kipkoech <Mail.Vincentk@gmail.com>
2024-06-13 21:43:55 -0500

    Days to reach Mars and Moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,22 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+const milesPerKm = 0.621;
+console.log( typeof shuttleName);
+console.log( typeof shuttleSpeedMph);
+console.log( typeof distanceToMarsKm);
+console.log( typeof distanceToMarsKm);
+console.log( typeof milesPerKm);
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
+console.log(shuttleName + " will take " + daysToMars + " days to reach the Mars.");
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
