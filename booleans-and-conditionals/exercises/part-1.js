a0ffa3b5d984467b54aa3e2efb58feba595fa287
Vincent Kipkoech <Mail.Vincentk@gmail.com>
2024-06-17 14:59:15 -0500

    Class 2

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,5 +1,14 @@
 // Declare and initialize the variables for exercise 1 here:
-
+/**/
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+let fuelLevel = 18000;
+let engineTemperature = 2500;
+let commandOverride = false;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
