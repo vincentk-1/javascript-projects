e704f385f4f57e052536a049c4cfad82f8485c0a
Vincent Kipkoech <Mail.Vincentk@gmail.com>
2024-08-05 23:15:16 -0500

    The DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,8 +2,31 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
+    const button2 = document.getElementById("abortMission"); 
     // Put your code for the exercises here.
+ 
+    button.addEventListener('click', event => {
+    paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener("mouseover", function( event ) {
+    event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function( event ) {
+    event.target.style.backgroundColor = "";
+    });
+
+    button2.addEventListener('click', event => {
+    let response = window.confirm("Are you sure you want to abort the mission?");
+
+    console.log(response)
+    if (response === true)
+    {
+    paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+    }
+    });
+
     
 }
 
